c7b9173406cb5e278d3028b6012ce3d3a1d27879
Ilya Pushkarev <ilyapushkarev10@gmail.com>
2020-07-29 12:33:49 +0300

    1

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,7 +3,8 @@ import  axios from "axios";
      baseURL:`https://social-network.samuraijs.com/api/1.0/`,
      withCredentials: true,
     headers: {
-        "API-KEY": "0d0f2a57-8c82-4777-9ae0-92794f8f6390",
+        // "API-KEY": "0d0f2a57-8c82-4777-9ae0-92794f8f6390",
+        "API-KEY": "f403d1c7-2c58-4a4b-bbc9-53894107316e",
     },
 });
 export default API;
